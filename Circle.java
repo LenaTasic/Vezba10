bc4e8a44be77fd735c0694c262ee1ca0762b48c6
LenaTasic <97707992+LenaTasic@users.noreply.github.com>
2023-12-08 13:54:45 -0500

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Shape {
+    int x,y;
+    String color,name;
+
+    public void calculateArea(){
+        System.out.println("Calculate Area for a Shape: ");
+    }
+
+
+}
