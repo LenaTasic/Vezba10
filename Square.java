5bc57db4df84dd0cc4f095dda16b0147c50195da
LenaTasic <97707992+LenaTasic@users.noreply.github.com>
2023-12-08 13:55:09 -0500

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Square extends Shape{
+
+    int a;
+
+    @Override
+    public void calculateArea(){
+        System.out.println("Square Area: "+(a*a));
+
+
+    }
+
+}
